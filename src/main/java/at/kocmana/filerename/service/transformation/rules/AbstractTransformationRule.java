bcacb67a1fcbdc24fc882af81f588979af5bb409
kocmana <andreas.kocman@gmail.com>
2022-01-10 23:13:21 +0100

    WIP Extracted further common logic into AbstractTransformationRule

--- src/main/java/at/kocmana/filerename/service/transformation/rules/AbstractTransformationRule.java
+++ src/main/java/at/kocmana/filerename/service/transformation/rules/AbstractTransformationRule.java
@@ -1,39 +1,67 @@
 package at.kocmana.filerename.service.transformation.rules;
 
+import java.nio.file.Path;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class AbstractTransformationRule implements TransformationRule {
 
   private static final String RULE_EXTRACTION_LEADING_TEMPLATE = "(?<leading>.*?)(";
   private static final String RULE_EXTRACTION_TRAILING_TEMPLATE = ")(?<trailing>.*?)\\.(?<fileSuffix>.+)\\b";
-  private static final String RULE_START_MARKER = "<{2}";
-  private static final String RULE_END_MARKER = ">{2}";
+  private static final String RULE_START_MARKER = "(?<rule>\\<{2}";
   private static final String RULE_ARGUMENTS_MARKER = "\\(|(?<ruleArguments>.+))?";
+  private static final String RULE_END_MARKER = "\\>{2})";
 
-  private final String ruleMarker;
+  private final String ruleExtractionTemplate;
+  private final Pattern ruleExtractionPattern;
+
+  private final String ruleShortcut;
+  private final String genericRulePattern;
   private final String inputRuleArguments;
   private final String outputRuleArguments;
-  private String ruleExtractionTemplate;
-  private Pattern ruleExtractionPattern;
+  private final String filenameRuleGroupName;
 
   private String inputFilenamePattern;
   private String outputFilenamePattern;
 
-  protected AbstractTransformationRule(String templateShortcut, String inputFilenamePattern, String outputFilenamePattern) {
-    this.ruleMarker = templateShortcut;
-    ruleExtractionTemplate = generateTemplateExtractionPattern(templateShortcut);
+  private final TransformationRuleIdentity identity;
+
+  protected AbstractTransformationRule(String ruleShortcut, String inputFilenamePattern, String outputFilenamePattern) {
+    this.ruleShortcut = ruleShortcut;
+    ruleExtractionTemplate = generateTemplateExtractionPattern(ruleShortcut);
     ruleExtractionPattern = Pattern.compile(ruleExtractionTemplate);
-    inputRuleArguments = extractRuleArguments(inputFilenamePattern);
-    outputRuleArguments = extractRuleArguments(outputFilenamePattern);
+    genericRulePattern = generateGroupPattern(ruleShortcut);
+    filenameRuleGroupName = generateFilenameGroupPattern(ruleShortcut);
+    inputRuleArguments = extractGroup(inputFilenamePattern);
+    outputRuleArguments = extractGroup(outputFilenamePattern);
+    identity = generateIdentity();
   }
 
-  private String extractRuleArguments(String filenamePattern) {
-    var matcher = ruleExtractionPattern.matcher(inputFilenamePattern);
+  private String generateGroupPattern(String ruleShortcut) {
+    return String.format("\\<{2}%s(|.*)?\\>{2}", ruleShortcut);
+  }
+
+  private String generateFilenameGroupPattern(String ruleShortcut) {
+    return String.format("?<%s.*>", ruleShortcut);
+  }
+
+  private String extractGroup(String filenamePattern) {
+    Matcher matcher = matcherFor(filenamePattern);
+    return matcher.group("ruleArguments");
+  }
+
+  private TransformationRuleIdentity generateIdentity() {
+    var matcher = matcherFor(outputFilenamePattern);
+    return new TransformationRuleIdentity(matcher.start("rule"), matcher.end("rule"));
+  }
+
+  private Matcher matcherFor(String filenamePattern) {
+    var matcher = ruleExtractionPattern.matcher(filenamePattern);
     if (!matcher.find()) {
-      var message = String.format("Filename pattern \"%s\" does not contain rule marker \"%s\".", filenamePattern, ruleMarker);
+      var message = String.format("Filename pattern \"%s\" does not contain rule marker \"%s\".", filenamePattern, ruleShortcut);
       throw new IllegalArgumentException(message);
     }
-    return matcher.group("ruleArguments");
+    return matcher;
   }
 
   private static String generateTemplateExtractionPattern(String templateShortcut) {
@@ -46,14 +74,36 @@ public abstract class AbstractTransformationRule implements TransformationRule {
             + RULE_EXTRACTION_TRAILING_TEMPLATE;
   }
 
-  protected static boolean ruleMarkerIsPresent(String ruleMarker, String filenamePattern) {
-    var ruleExtractionPattern = Pattern.compile(generateTemplateExtractionPattern(ruleMarker));
+  protected static boolean ruleMarkerIsPresent(String ruleShortcut, String filenamePattern) {
+    var ruleExtractionPattern = Pattern.compile(generateTemplateExtractionPattern(ruleShortcut));
     var matcher = ruleExtractionPattern.matcher(filenamePattern);
     return matcher.find();
   }
 
-  public String getRuleMarker() {
-    return ruleMarker;
+  @Override
+  public String replaceTemplateWithSearchString(String pattern) {
+    return pattern.replaceAll(genericRulePattern, filenameRuleGroupName);
+  }
+
+  @Override
+  public String apply(Path file, String outputPattern) {
+    var filename = file.getFileName().toString();
+    return apply(filename, outputPattern);
+  }
+
+  protected String apply(String file, String outputPattern) {
+    var message = String.format("apply(String, String) method not implemented for \"%s\" and apply(Path, String)"
+            + " method not overriden.", this.getClass().getSimpleName());
+    throw new ExceptionInInitializerError(message); //TODO create own MethodNotImplemented Exception
+  }
+
+  @Override
+  public TransformationRuleIdentity getIdentity() {
+    return identity;
+  }
+
+  public String getRuleShortcut() {
+    return ruleShortcut;
   }
 
   public String getInputRuleArguments() {
@@ -70,6 +120,14 @@ public abstract class AbstractTransformationRule implements TransformationRule {
 
   public String getOutputFilenamePattern() {
     return outputFilenamePattern;
+  }
+
+  protected String getGenericRulePattern() {
+    return genericRulePattern;
+  }
+
+  protected String getFilenameRuleGroupName() {
+    return filenameRuleGroupName;
   }
 
 }
