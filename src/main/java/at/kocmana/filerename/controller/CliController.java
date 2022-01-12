591d0d34327304126d67fe634c81eae1b147280c
kocmana <andreas.kocman@gmail.com>
2022-01-12 22:03:58 +0100

    #3 Add support for multiple file rename tasks

--- src/main/java/at/kocmana/filerename/controller/CliController.java
+++ src/main/java/at/kocmana/filerename/controller/CliController.java
@@ -2,54 +2,71 @@ package at.kocmana.filerename.controller;
 
 import at.kocmana.filerename.model.CommandLineArguments;
 import at.kocmana.filerename.service.FileRenameTask;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
+import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.CompletableFuture;
 
 @Command(mixinStandardHelpOptions = true, versionProvider = VersionInformationController.class)
 public class CliController implements Runnable {
 
   private static final Logger log = LoggerFactory.getLogger(CliController.class);
 
-  @Option(names = {"-p", "--path"},
-          description = "The directory for the operation")
-  private Path path = Paths.get(".");
+  @ArgGroup(exclusive = false, multiplicity = "1..*")
+  List<CliArgument> cliArguments;
 
-  @Option(names = {"-r", "--recursive"},
-          description = "Include sub directories.", defaultValue = "false")
-  private boolean recursive = false;
+  static class CliArgument {
+    @Option(names = {"-p", "--path"},
+        description = "The directory for the operation")
+    private Path path = Paths.get(".");
 
-  @Option(names = {"-i", "--input"}, required = true,
-          description = "The pattern of the input file names")
-  private String inputTemplate;
+    @Option(names = {"-r", "--recursive"},
+        description = "Include sub directories.", defaultValue = "false")
+    private boolean recursive = false;
 
-  @Option(names = {"-o", "--output"}, required = true,
-          description = "The pattern of the output file names")
-  private String outputTemplate;
+    @Option(names = {"-i", "--input"}, required = true,
+        description = "The pattern of the input file names")
+    private String inputTemplate;
 
-  @Option(names = {"-d", "--dryRun"},
-          description = "Setting this parameter will only display how the file names will be changed", defaultValue = "false")
-  private boolean dryRun = false;
+    @Option(names = {"-o", "--output"}, required = true,
+        description = "The pattern of the output file names")
+    private String outputTemplate;
+
+    @Option(names = {"-d", "--dryRun"},
+        description = "Setting this parameter will only display how the file names will be changed", defaultValue = "false")
+    private boolean dryRun = false;
+  }
 
   @Override
   public void run() {
     var arguments = mapArguments();
     try {
-      var taskStatusFuture = CompletableFuture.supplyAsync(() -> new FileRenameTask(arguments).call());
-      log.info("Process finished with status {}", taskStatusFuture.get());
+      var completableFutures = arguments.stream()
+          .map(FileRenameTask::new)
+          .map(task -> CompletableFuture.supplyAsync(() -> task.call()))
+          .toList();
+
+      var allTasksFutures = CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]));
+
+      allTasksFutures.get();
+
+      log.info("Process finished.");
     } catch (Exception exception) {
       log.error("Encountered issue while running the application: {}.", exception.getMessage(), exception);
     }
   }
 
-  CommandLineArguments mapArguments() {
-    return new CommandLineArguments(path, recursive, inputTemplate, outputTemplate, dryRun);
+  List<CommandLineArguments> mapArguments() {
+    return cliArguments.stream()
+        .map(args -> new CommandLineArguments(args.path, args.recursive, args.inputTemplate, args.outputTemplate,
+            args.dryRun))
+        .toList();
   }
 
   public void run(String[] args) {
--- src/test/java/at/kocmana/filerename/controller/CliControllerTest.java
+++ src/test/java/at/kocmana/filerename/controller/CliControllerTest.java
@@ -3,134 +3,162 @@ package at.kocmana.filerename.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import at.kocmana.filerename.controller.CliController;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import picocli.CommandLine;
 
-import java.nio.file.Path;
-
 class CliControllerTest {
-
-  private static final CliController UNDER_TEST = new CliController();
 
   @ParameterizedTest
   @ValueSource(strings = {"-i", "--input"})
   void testCorrectInputPattern(String parameterName) {
     //given
+    var underTest = new CliController();
     String[] args = {parameterName, "foo", "-o", "bar"};
 
     //when
-    new CommandLine(UNDER_TEST).parseArgs(args);
-    var actualResult = UNDER_TEST.mapArguments();
+    new CommandLine(underTest).parseArgs(args);
+    var actualResult = underTest.mapArguments();
 
     //then
-    assertThat(actualResult.inputTemplate()).isEqualTo("foo");
+    assertThat(actualResult).hasSize(1);
+    assertThat(actualResult.get(0).inputTemplate()).isEqualTo("foo");
   }
 
   @Test
   void testExceptionOnMissingInputPattern() {
     //given
+    var underTest = new CliController();
     String[] args = {"-o", "bar"};
 
     //when + then
     assertThatExceptionOfType(CommandLine.MissingParameterException.class)
-        .isThrownBy(() -> new CommandLine(UNDER_TEST).parseArgs(args))
-        .withMessage("Missing required option: '--input=<inputTemplate>'");
+        .isThrownBy(() -> new CommandLine(underTest).parseArgs(args))
+        .withMessage("Error: Missing required argument(s): --input=<inputTemplate>");
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"-o", "--output"})
   void testCorrectOutputPattern(String parameterName) {
     //given
+    var underTest = new CliController();
     String[] args = {"-i", "foo", parameterName, "bar"};
 
     //when
-    new CommandLine(UNDER_TEST).parseArgs(args);
-    var actualResult = UNDER_TEST.mapArguments();
+    new CommandLine(underTest).parseArgs(args);
+    var actualResult = underTest.mapArguments();
 
     //then
-    assertThat(actualResult.outputTemplate()).isEqualTo("bar");
+    assertThat(actualResult).hasSize(1);
+    assertThat(actualResult.get(0).outputTemplate()).isEqualTo("bar");
   }
 
   @Test
   void testExceptionOnMissingOutputPattern() {
     //given
+    var underTest = new CliController();
     String[] args = {"-i", "foo"};
 
     //when + then
     assertThatExceptionOfType(CommandLine.MissingParameterException.class)
-        .isThrownBy(() -> new CommandLine(UNDER_TEST).parseArgs(args))
-        .withMessage("Missing required option: '--output=<outputTemplate>'");
+        .isThrownBy(() -> new CommandLine(underTest).parseArgs(args))
+        .withMessage("Error: Missing required argument(s): --output=<outputTemplate>");
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"-r", "--recursive"})
   void testCorrectRecursiveArgumentValue(String parameterName) {
     //given
+    var underTest = new CliController();
     String[] args = {"-i", "foo", "-o", "bar", parameterName};
 
     //when
-    new CommandLine(UNDER_TEST).parseArgs(args);
-    var actualResult = UNDER_TEST.mapArguments();
+    new CommandLine(underTest).parseArgs(args);
+    var actualResult = underTest.mapArguments();
 
     //then
-    assertThat(actualResult.recursive()).isTrue();
+    assertThat(actualResult).hasSize(1);
+    assertThat(actualResult.get(0).recursive()).isTrue();
   }
 
   @Test
   void testMissingRecursiveArgumentValue() {
     //given
+    var underTest = new CliController();
     String[] args = {"-i", "foo", "-o", "bar"};
 
     //when
-    new CommandLine(UNDER_TEST).parseArgs(args);
-    var actualResult = UNDER_TEST.mapArguments();
+    new CommandLine(underTest).parseArgs(args);
+    var actualResult = underTest.mapArguments();
 
     //then
-    assertThat(actualResult.recursive()).isFalse();
+    assertThat(actualResult).hasSize(1);
+    assertThat(actualResult.get(0).recursive()).isFalse();
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"-d", "--dryRun"})
   void testCorrectDryRunArgumentValue(String parameterName) {
     //given
+    var underTest = new CliController();
     String[] args = {"-i", "foo", "-o", "bar", parameterName};
 
     //when
-    new CommandLine(UNDER_TEST).parseArgs(args);
-    var actualResult = UNDER_TEST.mapArguments();
+    new CommandLine(underTest).parseArgs(args);
+    var actualResult = underTest.mapArguments();
 
     //then
-    assertThat(actualResult.dryRun()).isTrue();
+    assertThat(actualResult).hasSize(1);
+    assertThat(actualResult.get(0).dryRun()).isTrue();
   }
 
   @Test
   void testMissingDryRunArgumentValue() {
     //given
+    var underTest = new CliController();
     String[] args = {"-i", "foo", "-o", "bar"};
 
     //when
-    new CommandLine(UNDER_TEST).parseArgs(args);
-    var actualResult = UNDER_TEST.mapArguments();
+    new CommandLine(underTest).parseArgs(args);
+    var actualResult = underTest.mapArguments();
 
     //then
-    assertThat(actualResult.dryRun()).isFalse();
+    assertThat(actualResult).hasSize(1);
+    assertThat(actualResult.get(0).dryRun()).isFalse();
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"-p", "--path"})
   void testCorrectPathArgumentValue(String parameterName) {
     //given
+    var underTest = new CliController();
     String[] args = {"-i", "foo", "-o", "bar", parameterName, "./baz"};
 
     //when
-    new CommandLine(UNDER_TEST).parseArgs(args);
-    var actualResult = UNDER_TEST.mapArguments();
+    new CommandLine(underTest).parseArgs(args);
+    var actualResult = underTest.mapArguments();
 
     //then
-    assertThat(actualResult.path()).endsWithRaw(Path.of("baz"));
+    assertThat(actualResult).hasSize(1);
+    assertThat(actualResult.get(0).path()).endsWithRaw(Path.of("baz"));
+  }
+
+  @Test
+  void testMultipleArgumentGroups() {
+    //given
+    var underTest = new CliController();
+    String[] args = {"-i", "foo", "-o", "bar", "-i", "any", "-o", "some"};
+
+    //when
+    new CommandLine(underTest).parseArgs(args);
+    var actualResult = underTest.mapArguments();
+
+    //then
+    assertThat(actualResult).hasSize(2)
+        .anyMatch(argument -> argument.inputTemplate().equals("foo"))
+        .anyMatch(argument -> argument.inputTemplate().equals("any"));
   }
 
 }
