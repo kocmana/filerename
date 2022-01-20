0cd9e9f953ada73881c86593c27e4a8eb21b681c
kocmana <andreas.kocman@gmail.com>
2022-01-20 20:49:01 +0100

    #5 Add filename collision resolution - WIP

--- src/main/java/at/kocmana/filerename/model/CommandLineArguments.java
+++ src/main/java/at/kocmana/filerename/model/CommandLineArguments.java
@@ -1,13 +1,16 @@
 package at.kocmana.filerename.model;
 
+import at.kocmana.filerename.controller.CliController.CollisionResolutionStrategy;
+
 import java.nio.file.Path;
 
 public record CommandLineArguments(
-    Path path,
-    boolean recursive,
-    String inputTemplate,
-    String outputTemplate,
-    boolean dryRun,
-    boolean createCopy
+        Path path,
+        boolean recursive,
+        String inputTemplate,
+        String outputTemplate,
+        boolean dryRun,
+        boolean createCopy,
+        CollisionResolutionStrategy collisionResolutionStrategy
 ) {
 }
